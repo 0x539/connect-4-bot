c48250e2fe412877344ce21087aacd2fc1f12428
0x539 <omgbloodypass@gmail.com>
2016-05-06 22:57:29 +0100

    Initial

---
+++ Strategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FourInARow
+{
+
+    public interface IStrategy
+    {
+        int NextMove(Board board);
+    }
+
+    public class Strategy : IStrategy
+    {
+        public int NextMove(Board board)
+        {
+            //TODO: write your code to choose best move on current board
+            Random r = new Random();
+            var col = r.Next(board.ColsNumber());
+            return col;
+        }
+    }
+
+}
